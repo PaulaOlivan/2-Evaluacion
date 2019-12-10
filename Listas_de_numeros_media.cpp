c822f788b8b1d59ea300f5dc4c20830e30c882ae
Paula Olivan <55576202+PaulaOlivan@users.noreply.github.com>
2019-12-10 11:41:53 +0100

    Add files via upload

---
+++ Listas_de_numeros_media.cpp
@@ -0,0 +1,37 @@
+//programa que lee una lista de caracteres (palabra) o numeros (numero de mas de una cifra)
+#include<iostream>
+/*Este programa pedira un numero de numeros que guardara y nos dara la media aritmetica del conjunto de numeros*/
+int main(){
+    char salir;
+    int num[4];
+    int cont;
+    float suma_num; //Si luego se dividen dos numeros enteros nunca dara numero decimal, por lo que hay que hacer que uno sea decimal, aunque metamos numero entero
+    float media;   //A esto se le llama conversion forzada
+    //Introducimos los numeros en el vector
+    for(cont=0;cont<=3;cont++){
+          std::cout<<"Introduce el numero: ", cont;
+          std::cin>> num[cont];
+    }
+    //Sacamos los numeros introducidos en el vector
+    std::cout<<"Has introducido los siguientes numeros: ";
+    for(cont=0;cont<=3;cont++){ //Como el num[1] es igual a cero, solo se llega hasta num[3] no al cuatro, ya que seria 5 numeros en ese caso
+    std::cout<<num[cont];
+    std::cout<<", ";                         
+    }
+    std::cout<<"\n";
+    //calculamos la media
+    std::cout<<"*MEDIA*\n";
+    suma_num=0;
+    for(cont=0; cont<=3; cont++){
+                //suma acumulada o variable acumuladora
+                suma_num=suma_num+num[cont];            
+    }
+    std::cout<<"La suma de los numeros vale ";
+    std::cout<< suma_num;
+    media=suma_num/cont;
+    std::cout<<". Por lo que la media vale ";
+    std::cout<< media;
+    std::cout<<".";
+    
+    std::cin>>salir;
+}
