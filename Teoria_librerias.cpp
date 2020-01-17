ba16803dc222651e397a43d8f82b2c677ba2a59b
Paula Olivan <55576202+PaulaOlivan@users.noreply.github.com>
2020-01-17 09:23:11 +0100

    Add files via upload

---
+++ Teoria_librerias.cpp
@@ -0,0 +1,16 @@
+/*Crear una libreria de cabecera
+Las instrucciones al preprocesador son todas las instrucciones que se colocan por delante del cuerpo pirncipal, son las que creamos en el cambiador de u
+      Ej:  #include<iostream> 
+int devuelve_longitud(){ --> es una funcion que devuelve algo
+    return(longitud)
+    
+void mayus_minus (){ --> es un procedimiento, se nombra con void y no devuelve nada
+     
+    
+int main(){ --> es el cuerpo principal
+
+SI EL CUERPO PRINCIPAL NO SE CIERRA, AL FINAL DE int main  SE PONE return(0)
+int main(){
+    return(0);
+*/
+      
