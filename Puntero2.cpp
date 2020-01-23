6d40b2b08464577f1511ee237060a15c418a5b89
Paula Olivan <55576202+PaulaOlivan@users.noreply.github.com>
2020-01-23 09:50:44 +0100

    Add files via upload

---
+++ Puntero2.cpp
@@ -0,0 +1,8 @@
+/*punteros 2.0
+  vamos a hacer un programa de punteros que nos permita realizar una operacion de manera mas dinamica
+  de esta manera podemos reservar exclusivamente el hueco de memoria que vamos a utilizar, de manera que no perdemos memoria RAM al reservar espacio que no vamos a utilizar
+*/ 
+
+//Este programa lee 4 nombre sin importar el numero de letras que tiene cada uno, guardara su posicion y luego lo recuperaremos
+
+
